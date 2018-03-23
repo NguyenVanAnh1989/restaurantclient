a8587f0a86da044b9b3cdecf4265d154763bb97c
anhnvd00205 <anhnvd0025@fpt.edu.vn>
2018-03-23 23:06:35 +0700

    add init project

---
+++ src/main/java/com/domain/ClientRestaurantApplication.java
@@ -0,0 +1,12 @@
+package com.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRestaurantApplication.class, args);
+	}
+}
